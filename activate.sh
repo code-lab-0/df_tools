986f7d603f1ccfea1516d312904894fdf81d4352
Yoshihiro Okuda <yookuda@nig.ac.jp>
2017-06-02 14:30:22 +0900

    activate.sh中のエイリアスをシェル関数に変更

--- activate.sh
+++ activate.sh
@@ -12,16 +12,29 @@ is_defined() {
 
 export DF_TOOLS=$HOME/works/df_tools
 export PATH=$PATH:$DF_TOOLS
-export DATACELL_MYSQL_JAR=$DF_TOOLS/DataCell-mysql-8.1.1-jar-with-dependencies.jar
+export DATACELL_MYSQL_JAR=$DF_TOOLS/DataCell-mysql-8.1.2-jar-with-dependencies.jar
 
 
-alias dc_join='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcJoinMySQL "$@"'
-alias dc_list='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcListMySQL "$@"'
-alias dc_load='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcLoadMySQL "$@"'
-alias dc_delete='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcDeleteTablesMySQL "$@"'
+#alias dc_join='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcJoinMySQL "$@"'
+#alias dc_list='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcListMySQL "$@"'
+#alias dc_load='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcLoadMySQL "$@"'
+#alias dc_delete='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcDeleteTablesMySQL "$@"'
 
+dc_join() {
+    java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcJoinMySQL "$@"
+}
 
+dc_list() {
+    java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcListMySQL "$@"
+}
 
+dc_load() {
+    java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcLoadMySQL "$@"
+}
+
+dc_delete() {
+    java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcDeleteTablesMySQL "$@"
+}
 #
 #
 #
