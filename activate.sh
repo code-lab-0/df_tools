3181484a1603977e222c36095deff34b701b07bd
Yoshihiro Okuda <yookuda@nig.ac.jp>
2017-08-01 12:35:40 +0900

    シェル関数名に.myを追加

--- activate.sh
+++ activate.sh
@@ -12,30 +12,21 @@ is_defined() {
 
 export DF_TOOLS=$HOME/works/df_tools
 export PATH=$PATH:$DF_TOOLS
-export DATACELL_MYSQL_JAR=$DF_TOOLS/DataCell-mysql-8.1.2-jar-with-dependencies.jar
+export DATACELL_MYSQL_JAR=$DF_TOOLS/DataCell-mysql-1.0.0-jar-with-dependencies.jar
 
-
-#alias dc_join='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcJoinMySQL "$@"'
-#alias dc_list='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcListMySQL "$@"'
-#alias dc_load='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcLoadMySQL "$@"'
-#alias dc_delete='java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcDeleteTablesMySQL "$@"'
-
-dc_join() {
+dc_join.my() {
     java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcJoinMySQL "$@"
 }
 
-dc_list() {
+dc_list.my() {
     java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcListMySQL "$@"
 }
 
-dc_load() {
+dc_load.my() {
     java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcLoadMySQL "$@"
 }
 
-dc_delete() {
+dc_delete.my() {
     java -cp $DATACELL_MYSQL_JAR net.ogalab.datacell.mysql.app.DcDeleteTablesMySQL "$@"
 }
-#
-#
-#
 
